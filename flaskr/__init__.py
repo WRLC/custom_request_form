39464c555adb100ab6625ddcd013ea035564d63a
Tom Boone <boone@wrlc.org>
2021-01-27 17:40:47 -0500

    parse url query params except first/last name

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,11 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request as req
 from config import *
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from urllib.parse import urlparse
+
 
 def create_app(test_config=None):
     # create and configure the app
@@ -27,36 +29,46 @@ def create_app(test_config=None):
         pass
 
     class RequestForm(FlaskForm):
-        genre = StringField('rft.genre')
-        title = StringField('rft.title')
-        stitle = StringField('rft.stitle')
-        atitle = StringField('rft.atitle')
-        date = StringField('rft.pubdate')
-        month = StringField('rft.month')
-        volume = StringField('rft.volume')
-        issue = StringField('rft.issue')
-        number = StringField('rft.number')
-        epage = StringField('rft.epage')
-        spage = StringField('rft.spage')
-        edition = StringField('rft.edition')
-        isbn = StringField('rft.isbn')
-        eisbn = StringField('rft.eisbn')
-        aulast = StringField('rft.aulast')
-        aufirst = StringField('rft.aufirst')
-        auinit = StringField('rft.auinit')
-        pub = StringField('rft.pub')
-        publisher = StringField('rft.publisher')
-        place = StringField('rft.place')
-        doi = StringField('rft.doi')
+        rft_genre = StringField('rft.genre')
+        rft_title = StringField('rft.title')
+        rft_stitle = StringField('rft.stitle')
+        rft_atitle = StringField('rft.atitle')
+        rft_date = StringField('rft.pubdate')
+        rft_month = StringField('rft.month')
+        rft_volume = StringField('rft.volume')
+        rft_issue = StringField('rft.issue')
+        rft_number = StringField('rft.number')
+        rft_epage = StringField('rft.epage')
+        rft_spage = StringField('rft.spage')
+        rft_edition = StringField('rft.edition')
+        rft_isbn = StringField('rft.isbn')
+        rft_eisbn = StringField('rft.eisbn')
+        rft_aulast = StringField('rft.aulast')
+        rft_aufirst = StringField('rft.aufirst')
+        rft_auinit = StringField('rft.auinit')
+        rft_pub = StringField('rft.pub')
+        rft_publisher = StringField('rft.publisher')
+        rft_place = StringField('rft.place')
+        rft_doi = StringField('rft.doi')
         rfe_dat = StringField('rft.oclcnum')
         rfr_id = StringField('rfr_id')
         submit = SubmitField('Submit Request')
-
 
     # a simple page that says hello
     @app.route('/')
     def request():
         form = RequestForm()
-        return render_template('request.html', title='WRLC Custom Request Form', form=form)
+        # TODO: Parse URL params and pre-populate form fields
+        query = req.args
+        for k, v in query.items():
+            k = k.replace('.', '_')
+            if k == 'rft_au':
+                # for key, value in v:
+                form.rft_aulast.data = v
+                    #form.rft_aufirst.data = value[1]
+            else:
+                form[k].data = v
+
+        return render_template('request.html', title=query, form=form)
 
     return app
